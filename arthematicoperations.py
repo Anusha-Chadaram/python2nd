5718f5ed7b611b37438b222dd65f61b0f11e84e8
Anusha-Chadaram <125711613+Anusha-Chadaram@users.noreply.github.com>
2023-10-09 19:30:09 +0530

    Add files via upload

---
+++ arthematicoperations.py
@@ -0,0 +1,10 @@
+a=int(input("enter the a value"))
+b=int(input("enter the b value"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("addittion of two numbers is",c)
+print("subtraction of two numbers is",d)
+print("multiplication : ",e)
+print("division: ",f)
