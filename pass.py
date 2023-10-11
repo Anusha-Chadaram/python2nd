c86be7040e2843dd0f928018a70ea728395d0b67
Anusha-Chadaram <125711613+Anusha-Chadaram@users.noreply.github.com>
2023-10-11 19:01:28 +0530

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,10 @@
+sno=int(input("enter the number"))
+sname=input("enter the name")
+group=input("enter the group")
+s1=int(input("enter the s1 marks"))
+s2=int(input("enter the s2 marks"))
+s3=int(input("enter the s3 marks"))
+if(s1>=40 and s2>=40 and s3>=40):
+    print("pass")
+else:
+    print("fail")
