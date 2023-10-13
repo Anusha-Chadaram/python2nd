d0ab85152763f23bfeb476e936dcaf3cb3d884b2
Anusha-Chadaram <125711613+Anusha-Chadaram@users.noreply.github.com>
2023-10-13 19:44:20 +0530

    Add files via upload

---
+++ leap.py
@@ -0,0 +1,8 @@
+#leap year
+year=int(input("enter the year"))
+if(year%4==0 and year%100!=0 or year%400==0):
+    print("leap year")
+#elif(year%400==0):
+   # print("leap year")
+else:
+    print(" not leap year")
