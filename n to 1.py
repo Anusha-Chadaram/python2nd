382cbb4504b9c5040116a4071df276e464225c44
Anusha-Chadaram <125711613+Anusha-Chadaram@users.noreply.github.com>
2023-10-16 18:56:31 +0530

    Add files via upload

---
+++ n to 1.py
@@ -0,0 +1,7 @@
+
+#n to 1
+num = int(input("Please Enter any Number: "))
+i = num
+while ( i >= 1):
+    print (i)
+    i = i - 1
---
+++ numbers.py
@@ -0,0 +1,5 @@
+#1 to n numbers
+n=int(input("enter the numbers"))
+while(1<=n):
+    print(n)
+    n=n+1
