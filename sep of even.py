a7b82222164a9b6cbabb4efa5f94ae03165ef0f2
Anusha-Chadaram <125711613+Anusha-Chadaram@users.noreply.github.com>
2023-11-21 18:39:36 +0530

    Add files via upload

---
+++ sep of even.py
@@ -0,0 +1,13 @@
+#even odd separation
+n=int(input())
+l=list(map(int,input().split()))
+even=[]
+odd=[]
+for i in l:
+    if i%2==0:
+        even.append(i)
+    else:
+        odd.append(i)
+res=even+odd
+for i in res:
+    print(i,end=' ')
